e1646947fa37d17decf5420dd91ed0b355d33649
Datle487 <letrongdat487@gmail.com>
2024-10-08 14:36:00 +0700

    Change layout loading and update search, upload layouts

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Fragment } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { publicRoutes } from "./routes"
 import { DedaultLayout } from "./components/Layout"
@@ -9,7 +10,25 @@ function App() {
                 <Routes>
                     {publicRoutes.map((route, index) => {
                         const Page = route.component
-                        return <Route key={index} path={route.path} element={<Page />}></Route>
+                        let Layout = DedaultLayout
+                        
+                        if (route.layout) {
+                            Layout = route.layout 
+                        } else if (route.layout === null) {
+                            Layout = Fragment
+                        }
+                        
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            ></Route>
+                        )
                     })}
                 </Routes>
             </div>
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,1 +1,2 @@
 export { default as DedaultLayout } from './DefaultLayout' 
+export { default as HeaderOnly } from './HeaderOnly' 
