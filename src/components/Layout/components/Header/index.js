a63f222560daf1a10f432aa23d656259bcc2e0f2
Datle487 <letrongdat487@gmail.com>
2024-10-09 22:48:49 +0700

    Header: Update logo and search bar

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,15 +1,33 @@
-import classNames from 'classnames/bind';
+import classNames from 'classnames/bind'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
 import styles from './Header.module.scss'
+import images from '~/assets/images'
 
 const cx = classNames.bind(styles)
 
 function Header() {
-    return (<header className={cx('wrapper')}>
-                <div className={cx('content')}>
-                    {/* logo */}
-                    {/* search */}
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('content')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
                 </div>
-        </header>)
+                <div className={cx('search')}>
+                    <input
+                        className={cx('search-input')}
+                        type="text"
+                        placeholder="Search"
+                    />
+                    <button className={cx('search-icon')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                    </button>
+                </div>
+                <div className={cx('navbar')}></div>
+            </div>
+        </header>
+    )
 }
 
-export default Header;
+export default Header
