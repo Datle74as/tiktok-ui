42d79ea471dd4742e8ddc9811d1f395fb06dd87e
Datle487 <letrongdat487@gmail.com>
2024-10-10 16:29:41 +0700

    Header: css search bar and starting with search drop

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,29 +1,61 @@
+import React, { useEffect, useState } from 'react'
+import Tippy from '@tippyjs/react/headless'
 import classNames from 'classnames/bind'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import {
+    faCircleXmark,
+    faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons'
 
+import { Wrapper as PopperWrapper } from '~/components/Popper'
 import styles from './Header.module.scss'
 import images from '~/assets/images'
 
 const cx = classNames.bind(styles)
 
 function Header() {
+    const [accountResults, setAccountResults] = useState([])
+
+    useEffect(() => {
+        setTimeout(() => {
+            setAccountResults([1, 2, 3])
+        }, 3000)
+    }, [])
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('content')}>
                 <div className={cx('logo')}>
                     <img src={images.logo} alt="Tiktok" />
                 </div>
-                <div className={cx('search')}>
-                    <input
-                        className={cx('search-input')}
-                        type="text"
-                        placeholder="Search"
-                    />
-                    <button className={cx('search-icon')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
-                    </button>
-                </div>
+                <Tippy
+                    visible={accountResults.length > 0}
+                    interactive
+                    render={(attrs) => (
+                        <div
+                            className={cx('search-drop')}
+                            tabIndex="-1"
+                            {...attrs}
+                        >
+                            <PopperWrapper>My tippy box</PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input
+                            className={cx('search-input')}
+                            type="text"
+                            placeholder="Search"
+                        />
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                        <button className={cx('search-close-btn')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                    </div>
+                </Tippy>
+
                 <div className={cx('navbar')}></div>
             </div>
         </header>
